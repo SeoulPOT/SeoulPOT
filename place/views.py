536acd569e885f210014ef1591382fffbfa3ae8e
haerin <junghl11@naver.com>
2024-08-13 09:29:24 +0900

    place

--- place/views.py
+++ place/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def category(request):
+    gu = request.GET.get('gu')
+    category = request.GET.get('category') 
+
+    gu = gu if gu else '중구'
+    category = category if category else 'food'
+
+    # 테이블 명 바꿔야함
+    context = {
+        'shop' : shop_tb.object.filter(gu = gu, category = category)
+    }
+    return render(request, 'place/place.html', context)
+
+def get_spots_by_category(request):
+    gu = request.GET.get("gu")
+    category = request.GET.get('category')     
+
+    data = shop_tb.object.filter(gu = gu, category = category)
+
+    return JsonResponse(data, safe=False)
