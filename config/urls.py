e48f9a196142d32b7b832b4d9a76f07bc4cb359b
whdtlr98 <whdtlr98@naver.com>
2024-08-16 13:48:24 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from review_tb import views
+from review import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
