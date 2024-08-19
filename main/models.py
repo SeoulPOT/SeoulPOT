37e78e5ea4472b50823c04066ae1889c3755d279
eunjin <ejhahappy52@gmail.com>
2024-08-19 19:13:50 +0900

    main view, urls

--- main/models.py
+++ main/models.py
@@ -2,11 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class district_tb(models.Model):
-    district_id = models.IntegerField(primary_key=True)
-    district_name = models.CharField(max_length=255)
-    district_img = models.URLField(max_length=255)
-    district_desc = models.TextField()
-
-    def __str__(self):
-        return self.district_name
--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.views.decorators.http import require_http_methods
 import logging
 from django.http import JsonResponse
-from .models import district_tb
 
 # Create your views here.
 
@@ -24,29 +23,3 @@ def get_sever_time(request):
             "error": "internal_server_error",
             "message": "An unexpected error occurred on the server."
         }, status=500)
-
-@require_http_methods(["GET"])
-def get_districts(request):
-    try:
-
-        # 해당 언어에 맞게 구별 정보를 불러옴 (예시로 한국어로만 처리)
-        districts = district_tb.objects.all()
-        response_data = { #확인필요
-            "districts": [
-                {
-                    "district_id": district.id,
-                    "district_name": district.district_name,
-                    "district_img": district.district_img,
-                    "district_desc": district.district_desc
-                }
-                for district in districts
-            ]
-        }
-        return JsonResponse(response_data)
-    
-    except Exception as e:
-        logger.error(f"Error fetching districts: {str(e)}")
-        return JsonResponse({
-            "error": "bad_request",
-            "message": "The request is required and must be a valid language code."
-        }, status=400)
