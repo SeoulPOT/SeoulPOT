d6af19985e5059ce85abf3381f7655ce5cbbc8a7
whdtlr98 <whdtlr98@naver.com>
2024-08-23 20:34:08 +0900

    Update urls.py

--- review/urls.py
+++ review/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:place_id>/<str:place_category_cd>/', views.content_reviews, name='project-content_reviews'),
+    path('<int:place_id>/<str:place_category_cd>/<str:place_tag_cd>/', views.content_reviews, name='project-content_reviews'),
 ]
-
-
-#place_id를 받고 이동
