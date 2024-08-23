dffd7212a68181d8c448a491c8505674c8bd1df8
whdtlr98 <whdtlr98@naver.com>
2024-08-23 20:35:04 +0900

    Update models.py

--- review/models.py
+++ review/models.py
@@ -1,13 +1,24 @@
 
 from django.db import models
 
+class Code_tb(models.Model):
+    code = models.CharField(max_length=15, primary_key=True)
+    code_name = models.CharField(max_length=255)
+    
+    class Meta:
+        db_table = 'code_tb'
+
 class Place_tb(models.Model):
     place_id = models.IntegerField(primary_key=True)
     place_name = models.CharField(max_length=200)
     place_address = models.CharField(max_length=2000)
     place_phone = models.CharField(max_length=15)
+    place_review_num = models.IntegerField()
     place_operating_hours = models.CharField(max_length=200)
     place_entrance_fee = models.IntegerField(default=0)
+    place_desc = models.CharField(max_length=2000)
+    place_url = models.CharField(max_length=300)
+    place_feature = models.CharField(max_length=200)
     place_keyword_cd = models.CharField(max_length=200)
     place_category_cd = models.CharField(max_length=100)
     place_tag_cd = models.CharField(max_length=200)
@@ -30,7 +41,7 @@ class Review_tb(models.Model):
     review_advertising = models.BooleanField()
     review_with_tag_cd = models.CharField(max_length=255)  # 리뷰 태그 (예: "#with,#daily,#특징")
     review_daily_tag_cd = models.CharField(max_length=255)
-    review_feature_tag_cd = models.CharField(max_length=255)
+    
 
     def __str__(self):
         return f'Review for {self.place}'
