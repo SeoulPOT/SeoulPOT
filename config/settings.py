7cfe01929931b41800f63b1bbea95f129d23e5fc
mabasa-k <dkwcdr@gmail.com>
2024-08-28 09:25:32 +0900

    메인시간 화면 ,시간

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = 'django-insecure-msabi9_e7sw@y86fmyztm8-uni*%c$(bqv7x1zsy8!37nch@g1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = '/static/'
 STATIC_URL = 'static/'
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
--- main/views.py
+++ main/views.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from django.views.decorators.http import require_http_methods
 import logging
 from django.http import JsonResponse
+import pytz
 
+# 미국 동부 표준시(EST) 타임존 설정
+est = pytz.timezone('America/New_York')
+kst = pytz.timezone('Asia/Seoul')
 # Create your views here.
 
 # 로깅 설정
@@ -14,7 +18,7 @@ logger = logging.getLogger(__name__)
 def get_sever_time(request):
     try:
         context = {
-            "now_time" : datetime.now().isoformat()
+            "now_time" : datetime.now(kst).isoformat()
         }
         return render(request, 'main/main.html', context)
     except Exception as e:
