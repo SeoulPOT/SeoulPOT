5275cdfa38365cc4b4e47c8b7d646b4493c4b9ef
mabasa-k <dkwcdr@gmail.com>
2024-11-05 09:36:17 +0900

    - 이미지 필터 기준 변경

--- district/views.py
+++ district/views.py
@@ -74,7 +74,7 @@ def choose_district(district_id, place_category_cd, lang):
     photo_subquery = (
         ReviewTb.objects
         .filter(place_id=OuterRef('place_id'))
-        .exclude(review_photo='')
+        .filter(has_photo=True)  # has_photo 필드를 사용
         .order_by('-review_date')
         .values('review_photo')[:1]
     )
